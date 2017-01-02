687d4372ad15cb4220a914691de1917038551c86
hendriandaglos <baifernsh25>
2017-01-02 10:52:28 +0700

    update pasien
    update kode pelanggan dan taggal lahir (id = 421463 sd 521682) 2

--- query_rizal_update_pasien.php
+++ query_rizal_update_pasien.php
@@ -2,10 +2,10 @@
 
 include 'db.php';
 
-$select = $db_pasien->("SELECT kode_pelanggan,tgl_lahir FROM pasien_rizal WHERE id >= '421463' AND id <= '521682' ");
+$select = $db_pasien->("SELECT id,kode_pelanggan,tgl_lahir FROM pasien_rizal WHERE id >= '421463' AND id <= '521682' ");
 while($sd = mysqli_fetch_array($select))
 {
-	$update = $db_pasien->("UPDATE pelanggan SET kode_pelanggan = '$sd[kode_pelanggan]' , tgl_lahir = '$sd[tgl_lahir]' WHERE id >= '421463' AND id <= '521682'  ");
+	$update = $db_pasien->("UPDATE pelanggan SET kode_pelanggan = '$sd[kode_pelanggan]' , tgl_lahir = '$sd[tgl_lahir]' WHERE id = '$sd[id]'  ");
 }
 
 ?>
