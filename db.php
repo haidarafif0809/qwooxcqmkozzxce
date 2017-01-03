1b212167be3a6d52d18818083ca7ddb3e64dceb0
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-01-03 14:10:13 +0700

    db commit

--- db.php
+++ db.php
@@ -1,18 +1,14 @@
 <?php date_default_timezone_set("Asia/Jakarta");
 
-
-
-
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "local_simklinik";
+$dbname = "aplikasi_simklinik";
 
-$conn = mysqli_connect($servername, $username, $password, $db) or die("Connection failed: " . mysqli_connect_error());
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 
 // perintah untuk mengkoneksikan php ke database mysql
-
-$db = new mysqli('localhost','root','','local_simklinik');
+$db = new mysqli('localhost','root','','aplikasi_simklinik');
 
 
 // Check connection
@@ -21,23 +17,20 @@ if ($db->connect_error) {
 }
 
 
-
 $servername_pasien = "localhost";
 $username_pasien = "root";
 $password_pasien = "";
-$dbname_pasien = "local_simklinik";
+$dbname_pasien = "aplikasi_simklinik";
 
 $conn_pasien = mysqli_connect($servername_pasien, $username_pasien, $password_pasien, $dbname_pasien) or die("Connection failed: " . mysqli_connect_error());
 
 // perintah untuk mengkoneksikan php ke database mysql
-
-$db_pasien = new mysqli('localhost','root','','local_simklinik');
+$db_pasien = new mysqli('localhost','root','','aplikasi_simklinik');
 
 
 // Check connection
-if ($db->connect_error) {
+if ($db_pasien->connect_error) {
     die("Connection failed: " . $db_pasien->connect_error);
 }
-
 
 ?>
