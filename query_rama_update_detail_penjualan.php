2f6efd153b6c1cc3124021e8e961480d73504773
hendriandaglos <baifernsh25>
2017-01-03 16:31:09 +0700

    Insert Ulang detail_penjualan

--- query_rama_update_detail_penjualan.php
+++ query_rama_update_detail_penjualan.php
@@ -7,7 +7,7 @@ while ($dp = mysqli_fetch_array($select_detail_penjualan)) {
 
     $delete_detail_penjualan = $db->query("DELETE FROM detail_penjualan WHERE id = '$dp[id]' ");
     
-    $insert_detail_penjualan = "INSERT INTO detail_penjualan (no_faktur, tanggal, jam, kode_barang, nama_barang, jumlah_barang, asal_satuan,satuan, harga, subtotal, potongan, tax, sisa) VALUES ('$dp[no_faktur]', '$dp[tanggal]', '$dp[jam]', '$dp[kode_barang]','$dp[nama_barang]','$dp[jumlah_barang]','$dp[asal_satuan]','$dp[satuan]','$dp[harga]','$dp[subtotal]','$dp[potongan]','$dp[tax]', '$dp[sisa]')";
+    $insert_detail_penjualan = "INSERT INTO detail_penjualan (no_faktur, tanggal, jam, kode_barang, nama_barang, jumlah_barang, asal_satuan,satuan, harga, subtotal, potongan, tax, sisa, kode_meja) VALUES ('$dp[no_faktur]', '$dp[tanggal]', '$dp[jam]', '$dp[kode_barang]','$dp[nama_barang]','$dp[jumlah_barang]','$dp[asal_satuan]','$dp[satuan]','$dp[harga]','$dp[subtotal]','$dp[potongan]','$dp[tax]', '$dp[sisa]', '1')";
 
     if ($db->query($insert_detail_penjualan) === TRUE) {
         echo "SUKSES";
