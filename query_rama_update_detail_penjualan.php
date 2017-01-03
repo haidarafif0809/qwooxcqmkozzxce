e778005f7d135c025000edd0294e77e5eccd3ec9
hendriandaglos <baifernsh25>
2017-01-03 16:38:26 +0700

    Insert Ulang detail penjualan

--- query_rama_update_detail_penjualan.php
+++ query_rama_update_detail_penjualan.php
@@ -2,7 +2,7 @@
     include 'db.php';
     include_once 'sanitasi.php';
 
-$select_detail_penjualan = $db->query("SELECT * FROM detail_penjualan WHERE no_faktur = '2/JL/01/16'");
+$select_detail_penjualan = $db->query("SELECT * FROM detail_penjualan WHERE no_faktur = '2/JL/01/17'");
 while ($dp = mysqli_fetch_array($select_detail_penjualan)) {
 
     $delete_detail_penjualan = $db->query("DELETE FROM detail_penjualan WHERE id = '$dp[id]' ");
