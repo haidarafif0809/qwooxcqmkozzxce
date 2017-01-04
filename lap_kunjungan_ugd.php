20de36635b034a5fd2f02f61940bc69d277e3ef0
armun-sbm <arismunandarsbm@gmail.com>
2017-01-04 10:40:32 +0700

    laporan kunjungan pasien
    menambahkan cari berdasarkan penjamin pada laporan kunjungan (ri,rj,ugd)

--- lap_kunjungan_ugd.php
+++ lap_kunjungan_ugd.php
@@ -29,7 +29,7 @@ $out_rj = mysqli_fetch_array($select_rj);
         <a class="dropdown-item" id="nav_rm" href="#">No RM</a>
         <a class="dropdown-item" id="nav_tanggal" href="#">Tanggal</a>
         <a class="dropdown-item" id="nav_ex" href="#">No RM dan Tanggal</a>
-
+        <a class="dropdown-item" id="nav_penjamin" href="#">Penjamin dan Periode</a>
     </div>
 </div>
 
@@ -77,31 +77,73 @@ $out_rj = mysqli_fetch_array($select_rj);
 </span><!--Akhir span untuk cari bersarkan tanggal-->
 <br>
 
+<!--=====AWAL CARI BERDASARKAN PENJAMIN DAN PERIODE-->
+<span id="show_penjamin"><!--span PENJAMIN-->
+<form class="form-inline" role="form" id="form_tanggal">
+
+<div class="form-group"> 
+  <select type="text" name="penjamin" id="penjamin_" autocomplete="off" class="form-control" placeholder="Penjamin" >
+  <option>Pilih Penjamin</option>
+  <?php 
+    $pilih = $db->query("SELECT nama FROM penjamin ");
+    while ($iya = mysqli_fetch_array($pilih)) {
+      
+    echo "<option value=".$iya['nama'].">".$iya['nama']."</option>";
+  }
+   ?>
+  </select>
+  </div>
+
+<div class="form-group"> 
+  <input type="text" name="dari_tanggal_penj" id="dari_tanggal_penj" autocomplete="off" class="form-control tanggal_cari" placeholder="Dari Tanggal">
+</div>
+
+<div class="form-group"> 
+  <input type="text" name="sampai_tanggal_penj" id="sampai_tanggal_penj" autocomplete="off" class="form-control tanggal_cari" placeholder="Sampai Tanggal">
+</div>
+
+<button type="submit" name="submit" id="lihat_penj" class="btn btn-default" style="background-color:#0277bd"><i class="fa fa-eye"> </i> Lihat </button>
+</form>
+</span><!--Akhir span PENJAMIN-->
+<!--AKHIR CARI BERDASARKAN  PENJAMIN DAN PERIODE-->
 
 <br>
 <div class="table-responsive">
 <span id="result"><!--span untuk table-->       
     <table id="table_lap_rj" class="table table-bordered table-sm">
         <thead>
-
-			<th style='background-color: #4CAF50; color:white'> No RM </th>
-			<th style='background-color: #4CAF50; color:white'> No REG </th>
-			<th style='background-color: #4CAF50; color:white'> Nama Pasien </th>
-			<th style='background-color: #4CAF50; color:white'> Jenis Kelamin </th>
-			<th style='background-color: #4CAF50; color:white'> Umur </th>
-			<th style='background-color: #4CAF50; color:white'> Alamat Pasien </th>
-			<th style='background-color: #4CAF50; color:white'> Penjamin </th>
-			<th style='background-color: #4CAF50; color:white'> No Handphone </th>
-			<th style='background-color: #4CAF50; color:white'> Tanggal Periksa </th>
-
-            
-           </thead>
-
+    			<th style='background-color: #4CAF50; color:white'> No RM </th>
+    			<th style='background-color: #4CAF50; color:white'> No REG </th>
+    			<th style='background-color: #4CAF50; color:white'> Nama Pasien </th>
+    			<th style='background-color: #4CAF50; color:white'> Jenis Kelamin </th>
+    			<th style='background-color: #4CAF50; color:white'> Umur </th>
+    			<th style='background-color: #4CAF50; color:white'> Alamat Pasien </th>
+    			<th style='background-color: #4CAF50; color:white'> Penjamin </th>
+    			<th style='background-color: #4CAF50; color:white'> No Handphone </th>
+    			<th style='background-color: #4CAF50; color:white'> Tanggal Periksa </th>            
+        </thead>
      </table>
 </span><!--akhir span untuk table-->
 </div>
 
-
+<div class="table-responsive">
+<span id="berdasarkan_penjamin" style="display: none;"><!--span untuk table-->       
+    <table id="table_lap_penjamin" class="table table-bordered">
+        <thead>
+          <th style='background-color: #4CAF50; color:white'> No RM </th>
+          <th style='background-color: #4CAF50; color:white'> No REG </th>
+          <th style='background-color: #4CAF50; color:white'> Nama Pasien </th>
+          <th style='background-color: #4CAF50; color:white'> Jenis Kelamin </th>
+          <th style='background-color: #4CAF50; color:white'> Umur </th>
+          <th style='background-color: #4CAF50; color:white'> Alamat Pasien </th>
+          <th style='background-color: #4CAF50; color:white'> No Handphone </th>
+          <th style='background-color: #4CAF50; color:white'> Penjamin </th>
+          <th style='background-color: #4CAF50; color:white'> Tanggal Periksa </th>
+          <th style='background-color: #4CAF50; color:white'> Total </th>
+        </thead>
+     </table>
+</span><!--akhir span untuk table-->
+</div>
 
 <span id="download_exc">
 <a href='export_excel_kunjungan_pasien_rm_ugd.php' type='submit' id='btn_exc' class='btn btn-warning btn-lg'>Download Excel</a>
@@ -115,7 +157,9 @@ $out_rj = mysqli_fetch_array($select_rj);
 <a href='export_excel_ex_ugd.php' type='submit' id='btn_exc_rm_tanggal' class='btn btn-warning btn-lg'>Download Excel</a>
 </span>
 
-
+<span id="download_exc_penjamin">
+<a href='export_excel_ex_ugd_penjamin.php' type='submit' id='btn_exc_penjamin' class='btn btn-warning btn-lg'>Download Excel</a>
+</span>
 
 </div><!--Div Countainer-->
 
@@ -124,11 +168,16 @@ $out_rj = mysqli_fetch_array($select_rj);
   //berdasarkan no rm
   $(document).ready(function() {
 $(document).on('click','#lihat_rm',function(e) {
-        
+        $('#result').show();
+        $('#berdasarkan_penjamin').hide();
+
          $('#table_lap_rj').DataTable().destroy();
 
           var no_rm = $("#no_rm").val();
-
+          if (no_rm == '') {
+          alert("Silakan isi No. RM terlebih dahulu");
+        }
+        else{
           var dataTable = $('#table_lap_rj').DataTable( {
           "processing": true,
           "serverSide": true,
@@ -153,8 +202,9 @@ $(document).on('click','#lihat_rm',function(e) {
           }
     
         });
-
+      }
   $("#download_exc").show();
+  $("#download_exc_penjamin").hide();
    $("#download_exc_tanggal").hide();
   $("#download_exc_rm_tanggal").hide();
   $("#btn_exc").attr("href", "export_excel_kunjungan_pasien_ugd.php?no_rm="+no_rm+"");
@@ -172,13 +222,21 @@ $(document).on('click','#lihat_rm',function(e) {
 //berdasarkan tanggal
          $(document).ready(function() {
 $(document).on('click','#lihat_tanggal',function(e) {
-        
+       $('#result').show();
+        $('#berdasarkan_penjamin').hide(); 
+
          $('#table_lap_rj').DataTable().destroy();
 
         var dari_tanggal = $("#dari_tanggal").val();        
         var sampai_tanggal = $("#sampai_tanggal").val(); 
 
-
+        if (dari_tanggal == '') {
+          alert("Silakan tentukan dari tanggal terlebih dahulu");
+        }
+        else if (sampai_tanggal == '') {
+          alert("Silakan tentukan sampai tanggal terlebih dahulu");
+        }
+        else{
           var dataTable = $('#table_lap_rj').DataTable( {
           "processing": true,
           "serverSide": true,
@@ -204,10 +262,11 @@ $(document).on('click','#lihat_tanggal',function(e) {
           }
     
         });
-
+      }
   $("#download_exc_tanggal").show();
    $("#download_exc_rm_tanggal").hide();
     $("#download_exc").hide();
+    $("#download_exc_penjamin").hide();
   $("#btn_exc_tanggal").attr("href", "export_excel_kunjungan_pasien_tanggal_ugd.php?dari_tanggal="+dari_tanggal+"&sampai_tanggal="+sampai_tanggal+"");
 
         });
@@ -224,13 +283,24 @@ $(document).on('click','#lihat_tanggal',function(e) {
      //berdasrkan rm dan tanggal
         $(document).ready(function() {
 $(document).on('click','#lihat_ex',function(e) {
-        
+        $('#result').show();
+        $('#berdasarkan_penjamin').hide();
+
          $('#table_lap_rj').DataTable().destroy();
 
-          var no_rm = $("#no_rm_ex").val();
+        var no_rm = $("#no_rm_ex").val();
         var dari_tanggal = $("#dari_tanggal_ex").val();        
         var sampai_tanggal = $("#sampai_tanggal_ex").val(); 
-
+          if (no_rm == '') {
+          alert("Silakan isi No.RM terlebih dahulu");
+        }
+        else if (dari_tanggal == '') {
+          alert("Silakan tentukan dari tanggal terlebih dahulu");
+        }
+        else if (sampai_tanggal == '') {
+          alert("Silakan tentukan sampai tanggal terlebih dahulu");
+        }
+        else{
 
           var dataTable = $('#table_lap_rj').DataTable( {
           "processing": true,
@@ -258,10 +328,11 @@ $(document).on('click','#lihat_ex',function(e) {
           }
     
         });
-
+      }
   $("#download_exc_tanggal").hide();
    $("#download_exc_rm_tanggal").show();
     $("#download_exc").hide();
+    $("#download_exc_penjamin").hide();
   $("#btn_exc_rm_tanggal").attr("href", "export_excel_ex_rj.php?no_rm="+no_rm+"&dari_tanggal="+dari_tanggal+"&sampai_tanggal="+sampai_tanggal+"");
 
         });
@@ -274,7 +345,70 @@ $(document).on('click','#lihat_ex',function(e) {
 </script>
 
 
+<script type="text/javascript">
+     //berdasrkan Penjamin
+        $(document).ready(function() {
+      $(document).on('click','#lihat_penj',function(e) {
+        $('#result').hide();
+        $('#berdasarkan_penjamin').show();
 
+         $('#table_lap_penjamin').DataTable().destroy();
+
+        var penjamin = $("#penjamin_").val();
+        var dari_tanggal = $("#dari_tanggal_penj").val();        
+        var sampai_tanggal = $("#sampai_tanggal_penj").val(); 
+
+        if (penjamin == '') {
+          alert("Silakan isi penjamin terlebih dahulu");
+        }
+        else if (dari_tanggal == '') {
+          alert("Silakan tentukan dari tanggal terlebih dahulu");
+        }
+        else if (sampai_tanggal == '') {
+          alert("Silakan tentukan sampai tanggal terlebih dahulu");
+        }
+        else{
+          var dataTable = $('#table_lap_penjamin').DataTable( {
+          "processing": true,
+          "serverSide": true,
+          "info":     false,
+          "language": {
+        "emptyTable":     "My Custom Message On Empty Table"
+    },
+          "ajax":{
+            url :"datatable_lap_kunj_ugd_penjamin.php", // json datasource
+             "data": function ( d ) {
+                 d.penjamin    = $("#penjamin_").val();
+                d.dari_tanggal_penj = $("#dari_tanggal_penj").val();
+                d.sampai_tanggal_penj = $("#sampai_tanggal_penj").val();
+                // d.custom = $('#myInput').val();
+                // etc
+            },
+                type: "post",  // method  , by default get
+            error: function(){  // error handling
+              $(".tbody").html("");
+              $("#table_lap_penjamin").append('<tbody class="tbody"><tr><th colspan="3"></th></tr></tbody>');
+              $("#tableuser_processing").css("display","none");
+              
+            }
+          }
+    
+        });
+      }
+  $("#download_exc_tanggal").hide();
+   $("#download_exc_rm_tanggal").hide();
+    $("#download_exc").hide();
+    $("#download_exc_penjamin").show();
+  $("#btn_exc_penjamin").attr("href", "export_excel_ex_ugd_penjamin.php?penjamin="+penjamin+"&dari_tanggal="+dari_tanggal+"&sampai_tanggal="+sampai_tanggal+"");
+
+        });
+        $("form").submit(function(){
+        
+        return false;
+        
+        });  
+   });  
+</script>
 
 <script type="text/javascript">
       $(document).ready(function() {
@@ -301,11 +435,11 @@ $(document).on('click','#lihat_ex',function(e) {
       $("#show_no_rm").hide();
       $("#show_tanggal").hide();
       $("#show_ex").hide();
+      $("#show_penjamin").hide();
        $("#download_exc").hide();
+       $("#download_exc_penjamin").hide();
       $("#download_exc_tanggal").hide();
       $("#download_exc_rm_tanggal").hide();
-
-
   });
 </script>
 
@@ -315,19 +449,29 @@ $(document).ready(function(){
     $("#show_no_rm").show();
     $("#show_tanggal").hide();
     $("#show_ex").hide();
+    $("#show_penjamin").hide();
     });
 
     $("#nav_tanggal").click(function(){    
     $("#show_tanggal").show();  
     $("#show_no_rm").hide();
     $("#show_ex").hide();
+    $("#show_penjamin").hide();
     });
 
     $("#nav_ex").click(function(){
     $("#show_ex").show();  
     $("#show_tanggal").hide();  
     $("#show_no_rm").hide();
+    $("#show_penjamin").hide();
 	});
+
+    $("#nav_penjamin").click(function(){
+    $("#show_ex").hide();  
+    $("#show_tanggal").hide();  
+    $("#show_no_rm").hide();
+    $("#show_penjamin").show();
+  });
 
 });
 </script>
