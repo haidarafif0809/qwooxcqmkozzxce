6fd8892cbd30cb5e75d79181668b62be3d9b8e8f
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-01-12 10:56:43 +0700

    modal pencarian barang (pembelian)
    modal pencarian barang (pembelian)

--- modal_beli_baru.php
+++ modal_beli_baru.php
@@ -30,7 +30,7 @@ $columns = array(
 // getting total number records without any search
 $sql =" SELECT b.id, b.kode_barang, b.nama_barang, b.harga_beli, b.satuan, b.kategori, b.suplier, b.over_stok, b.stok_barang, s.nama ";
 $sql.=" FROM barang b LEFT JOIN satuan s ON b.satuan = s.id ";
-$sql.=" WHERE b.golongan_barang = 'Barang' OR b.berkaitan_dgn_stok = '' ";
+$sql.=" WHERE b.golongan_barang = 'Barang' OR b.berkaitan_dgn_stok = 'Barang'  ";
 
 $query = mysqli_query($conn, $sql) or die("Salahnya Disini 1");
 $totalData = mysqli_num_rows($query);
@@ -41,7 +41,7 @@ if( !empty($requestData['search']['value']) ) {
 // if there is a search parameter, $requestData['search']['value'] contains search parameter
    
 $sql =" SELECT b.id, b.kode_barang, b.nama_barang, b.harga_beli, b.satuan, b.kategori, b.suplier, b.over_stok, b.stok_barang, s.nama ";
-$sql.=" FROM barang b LEFT JOIN satuan s ON b.satuan = s.id WHERE b.golongan_barang = 'Barang'";
+$sql.=" FROM barang b LEFT JOIN satuan s ON b.satuan = s.id WHERE b.golongan_barang = 'Barang' OR b.berkaitan_dgn_stok = 'Barang' ";
 
     $sql.=" AND (b.kode_barang LIKE '".$requestData['search']['value']."%' ";    
     $sql.=" OR b.nama_barang LIKE '".$requestData['search']['value']."%' ";
