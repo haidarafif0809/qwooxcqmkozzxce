bf379c3871e2154f44fa1f9b1a0e0a87f2b03719
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-01-16 15:23:13 +0700

    query jurnal penjualan
    query jurnal penjualan

--- insert_jurnal_penjualan.php
+++ insert_jurnal_penjualan.php
@@ -6,7 +6,7 @@ include_once 'sanitasi.php';
 $mulai = $_GET['mulai'];
 
 //Mengambil data penjualan berdasarkan trtansaksi yang sudah '#LUNAS'
-$pilih_penjualan_tunai = $db->query("SELECT ppn, biaya_admin, user, tanggal, jam, cara_bayar, potongan, status, kredit, total, no_faktur, no_reg, kode_pelanggan FROM penjualan WHERE tanggal >= '2017-01-01' LIMIT $mulai,2");
+$pilih_penjualan_tunai = $db->query("SELECT ppn, biaya_admin, user, tanggal, jam, cara_bayar, potongan, status, kredit, total, no_faktur, no_reg, kode_pelanggan FROM penjualan WHERE tanggal = '2016-01-16' AND jam  >= '14:00:00' ");
 while ($data_penj = mysqli_fetch_array($pilih_penjualan_tunai)) { //START while ($data_penj) {
 
 
