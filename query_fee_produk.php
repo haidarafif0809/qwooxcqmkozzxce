25797ad72ea4248a8b11c1c3578460511ecb0def
RIkopernando <rindangramadhan10@gmail.com>
2017-01-27 18:29:29 +0700

    Update

--- query_fee_produk.php
+++ query_fee_produk.php
@@ -6,7 +6,7 @@ include 'db.php';
     $select = $db->query("SELECT * FROM penjualan WHERE tanggal = '2017-01-27'");
     while ($data = mysqli_fetch_array($select)) {
 
-      $query = $db->query("UPDATE laporan_fee_produk SET jam = '$data[jam]', waktu = '$data[tanggal] $data[jam]' WHERE no_faktur = '$data[no_faktur]'");
+      $query = $db->query("UPDATE laporan_fee_produk SET jam = '$data[jam]', waktu = '$data[tanggal] $data[jam]' WHERE no_faktur = '$data[no_faktur]' AND nama_petugas = '$data[dokter]'");
 
      
     }
