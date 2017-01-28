59f8a501f4aa388e41021af6abc2d7ec73111938
RIkopernando <rindangramadhan10@gmail.com>
2017-01-28 15:11:57 +0700

    Lap Fee Produk

--- query_fee_produk.php
+++ query_fee_produk.php
@@ -19,6 +19,9 @@ include 'db.php';
         $select = $db->query("SELECT * FROM detail_penjualan WHERE tanggal = '2017-01-27'");
         while ($data_det = mysqli_fetch_array($select)) {
 
+          $subtotaljadi = $data_det['subtotal'];
+          $jumlah = $data_det['jumlah_barang']
+
         // PERHITUNGAN UNTUK FEE DOKTER
         $ceking = $db->query("SELECT * FROM fee_produk WHERE nama_petugas = '$dokter' AND kode_produk = '$data_det[kode_barang]'");
         $cek_fee_dokter1 = mysqli_num_rows($ceking);
@@ -27,9 +30,7 @@ include 'db.php';
         if ($cek_fee_dokter1 > 0){
         if ($dataui['jumlah_prosentase'] != 0 AND $dataui['jumlah_uang'] == 0 )
 
-        {
-
-          
+        {      
 
 
 
@@ -253,14 +254,6 @@ include 'db.php';
 
         } // penutup if petugas di harga 1 > 0
         // ENDING PERHITUNGAN UNTUK FEE PETUGAS
-          $fee_produk_ksir = $db->query("SELECT * FROM tbs_fee_produk WHERE no_reg = '$data[no_reg]'");
-          while  ($data_fee_produk = mysqli_fetch_array($fee_produk_ksir)){
-
-
-                $query10 = $db->query("INSERT INTO laporan_fee_produk (nama_petugas, no_faktur, kode_produk, nama_produk, jumlah_fee, tanggal, jam, no_rm, no_reg) VALUES ('$data[nama_petugas]', '$data[no_faktur]', '$data_fee_produk[kode_produk]', '$data_fee_produk[nama_produk]', '$data_fee_produk[jumlah_fee]', '$data[tanggal]', '$data[jam]', '$data[kode_pelanggan]', '$data[no_reg]')");
-
-
-          }
 
 
 
