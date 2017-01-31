7771ec5fba1842a25abf6a53a6f78bc7de11b0ba
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-01-31 17:06:23 +0700

    Perbaikan file cek selisih
    Tambah selisih dan where nya

--- query_cek_penjualan_null.php
+++ query_cek_penjualan_null.php
@@ -6,6 +6,7 @@
 <td>Total Penjualan</td> 
 <td>Total Penjualan (asal)</td>
 <td>Total Detail Penjualan</td> 
+<td>Selisih</td> 
 </tr>
 <?php 
 include 'db.php';
@@ -14,17 +15,18 @@ include 'sanitasi.php';
 $dari_tanggal = stringdoang($_GET['dari_tanggal']);
 $sampai_tanggal = stringdoang($_GET['sampai_tanggal']);
 $tanggal = date('Y-m-d');
-$query = $db->query("SELECT no_faktur,total,tanggal,jam,potongan,tax FROM penjualan 	");
+$query = $db->query("SELECT no_faktur,total,tanggal,jam,potongan,tax FROM penjualan WHERE tanggal >= '$dari_tanggal'  AND tanggal <= '$sampai_tanggal'	");
 
 
 while ($data = $query->fetch_array()) {
 	# code...
 $total_asli = ($data['total'] + $data['potongan']) - $data['tax'];
+
 	$query_detail = $db->query("SELECT SUM(subtotal) AS subtotal FROM detail_penjualan WHERE  no_faktur = '$data[no_faktur]' GROUP BY no_faktur");
 
 	$v_detail = $query_detail->fetch_array();
-
-echo "<tr><td>".$data['no_faktur']."</td><td>".$data['tax']."</td><td>".$data['potongan']."</td><td>".$data['total']."</td><td>".$total_asli."</td><td>".$v_detail['subtotal']."</td></tr>";
+$selisih = $total_asli - $v_detail['subtotal'];
+echo "<tr><td>".$data['no_faktur']."</td><td>".$data['tax']."</td><td>".$data['potongan']."</td><td>".$data['total']."</td><td>".$total_asli."</td><td>".$v_detail['subtotal']."</td><td>".$selisih."</td></tr>";
 
 
 }
