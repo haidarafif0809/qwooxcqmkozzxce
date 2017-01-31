8e64e022fd75731c4f4f61cde6140f39efa8d9f1
RIkopernando <rindangramadhan10@gmail.com>
2017-01-31 10:36:49 +0700

    Revert "Input Obat Rawat Jalan"
    This reverts commit f8ac7917b40a8e65542158e61ed912e48702f65e.

--- lihat_produk_stok_opname.php
+++ lihat_produk_stok_opname.php
@@ -9,11 +9,6 @@ include 'sanitasi.php';
 
 $result = $db->query("SELECT * FROM barang WHERE nama_barang = '$nama_produk'");
 $row = mysqli_fetch_array($result);
-
-$select = $db->query("SELECT SUM(sisa) AS jumlah_barang FROM hpp_masuk WHERE kode_barang = '$row[kode_barang]'");
-$ambil_sisa = mysqli_fetch_array($select);
-$sisa_barang =$ambil_sisa['jumlah_barang'];
-$row['kategori'] = $sisa_barang;
    
     echo json_encode($row);
     exit;
