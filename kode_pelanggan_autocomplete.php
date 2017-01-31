dd80e210c02ab3a0e74ad7dfd287fb6bb6222e3d
Rikopernando <rikopernando96@gmail.com>
2017-01-31 10:10:59 +0700

    perbaikan auto complete pasien
    perbaikan auto complete pasien = > masih pakek db bukan db pasien

--- kode_pelanggan_autocomplete.php
+++ kode_pelanggan_autocomplete.php
@@ -7,7 +7,7 @@
     $searchTerm = $_GET['term'];
     
     //get matched data from skills table
-    $query = $db->query("SELECT kode_pelanggan, nama_pelanggan FROM pelanggan WHERE nama_pelanggan LIKE '%".$searchTerm."%' OR kode_pelanggan LIKE '%".$searchTerm."%' ORDER BY nama_pelanggan ASC");
+    $query = $db_pasien->query("SELECT kode_pelanggan, nama_pelanggan FROM pelanggan WHERE nama_pelanggan LIKE '%".$searchTerm."%' OR kode_pelanggan LIKE '%".$searchTerm."%' ORDER BY nama_pelanggan ASC");
     while ($row = $query->fetch_assoc()) {
         $data[] = $row['kode_pelanggan']."(".$row['nama_pelanggan'].")";
     }
