b3184629b2d041abac4d236fd9c4d6337040c8ae
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-01-31 12:08:09 +0700

    Tadi salah cek nya subtotal bayar ranap nya

--- cek_total_bayar_pesanan_barang.php
+++ cek_total_bayar_pesanan_barang.php
@@ -12,12 +12,11 @@ include 'sanitasi.php';
 // menampilakn hasil penjumlah subtotal ALIAS total penjualan dari tabel tbs_penjualan berdasarkan data no faktur
  $query = $db->query("SELECT SUM(subtotal) AS total_penjualan FROM tbs_penjualan WHERE no_faktur = '$no_faktur' AND no_reg = '$no_reg' ");
 
-  $total_ops = $db->query("SELECT SUM(harga_jual) AS total_operasi FROM tbs_operasi WHERE  no_reg = '$no_reg' ");
- $data_total_ops = mysqli_fetch_array($total_ops);
+
 
  // menyimpan data sementara yg ada pada $query
  $data = mysqli_fetch_array($query);
- $total = $data['total_penjualan'] + $data_total_ops['total_operasi'];
+ $total = $data['total_penjualan'] ;
 
 
 $a =  intval($total);
