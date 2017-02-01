54e91f34654ad25b4a809ccde3be7c798f0b0a4d
RIkopernando <rindangramadhan10@gmail.com>
2017-02-01 09:01:08 +0700

    Registrasi Pasien Baru
    -Saat blur (mengisi tanggal lahir) lalu langsung focus ke gol darah

--- rawat_jalan_baru.php
+++ rawat_jalan_baru.php
@@ -505,6 +505,7 @@ else
 <!--end script ambil data pasien modal-->
 
 
+
 <script type="text/javascript">
 // TANGGAL LAHIR UNTUK MENAMPILKAN UMUR
 $("#tanggal_lahir").blur(function(){
@@ -562,6 +563,7 @@ if (tanggal_lahir != '')
       var tanggal_lahir = $("#tanggal_lahir").val();
       var umur = hitung_umur(tanggal_lahir);
       $("#umur").val(umur);
+      $("#gol_darah").focus();
     }
     else if (tanggal_lahir == '')
     {
@@ -570,6 +572,7 @@ if (tanggal_lahir != '')
     else
     {
     $("#umur").val(umur);
+    $("#gol_darah").focus();
     }
 }
 
