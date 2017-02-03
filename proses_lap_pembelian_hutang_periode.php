a2175595e066a57de7b7311ee50e0a9af9486385
hendriandaglos <hendri.syand@gmail.com>
2017-02-03 09:48:25 +0700

    Laporan Piutang and Hutang
    Bisa cari berdasarkan periode atau under tanggal yang di cari...

--- proses_lap_pembelian_hutang_periode.php
+++ proses_lap_pembelian_hutang_periode.php
@@ -5,7 +5,8 @@ include 'db.php';
 
 /* Database connection end */
 
-$sampai_tanggal = stringdoang($_POST['sampai_tanggal']);
+$dari_tanggal = stringdoang($_POST['dari_tanggal_periode']);
+$sampai_tanggal = stringdoang($_POST['sampai_tanggal_periode']);
 
 // storing  request (ie, get/post) global array to a variable  
 $requestData= $_REQUEST;
@@ -18,42 +19,45 @@ $columns = array(
 	0=>'tanggal', 
 	1=>'tanggal_jt',
 	2=>'no_faktur',
-	3=>'kode_pelanggan',
+	3=>'nama',
 	4=>'total',
 	5=>'jam', 
 	6=>'user',
 	7=>'status',
 	8=>'potongan',
 	9=>'tax',
-	10=>'kredit', 
-	11=>'tunai',
-	12=>'id'
+	10=>'sisa', 
+	11=>'kredit',
+	12=>'nilai_kredit',
+	13=>'id'
+
 
 
 );
 
 // getting total number records without any search
-$sql = "SELECT pel.nama_pelanggan,p.id,p.penjamin,p.tanggal,p.tanggal_jt,p.no_faktur,p.kode_pelanggan,p.total,p.jam,p.user,p.status,p.potongan,p.tax,p.tunai,p.kredit";
-$sql.=" FROM penjualan p LEFT JOIN pelanggan pel ON p.kode_pelanggan = pel.kode_pelanggan ";
-$sql.=" WHERE p.tanggal <= '$sampai_tanggal' ";
-$sql.=" AND p.kredit != 0";
+$sql = "SELECT p.id,p.no_faktur,p.total,p.suplier,p.tanggal,p.tanggal_jt,p.jam,p.user,p.status,p.potongan,p.tax,p.sisa,p.kredit,p.nilai_kredit,s.nama,g.nama_gudang ";
+$sql.=" FROM pembelian p INNER JOIN suplier s ON p.suplier = s.id INNER JOIN gudang g ON p.kode_gudang = g.kode_gudang ";
+$sql.=" WHERE p.tanggal <= '$dari_tanggal' AND p.tanggal <= '$sampai_tanggal' ";
+$sql.=" AND kredit != 0";
 $sql.=" ";
 
 $query = mysqli_query($conn, $sql) or die("eror 1");
 $totalData = mysqli_num_rows($query);
 $totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
 
-if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
-$sql = "SELECT pel.nama_pelanggan,p.id,p.penjamin,p.tanggal,p.tanggal_jt,p.no_faktur,p.kode_pelanggan,p.total,p.jam,p.user,p.status,p.potongan,p.tax,p.tunai,p.kredit";
-$sql.=" FROM penjualan p LEFT JOIN pelanggan pel ON p.kode_pelanggan = pel.kode_pelanggan ";
-$sql.=" WHERE p.tanggal <= '$sampai_tanggal' ";
-$sql.=" AND p.kredit != 0";
 
-	$sql.=" AND ( p.no_faktur LIKE '".$requestData['search']['value']."%'";  
-	$sql.=" OR p.tanggal LIKE '".$requestData['search']['value']."%' ";
-	$sql.=" OR p.tanggal_jt LIKE '".$requestData['search']['value']."%'";  
-	$sql.=" OR p.penjamin LIKE '".$requestData['search']['value']."%'";   
-	$sql.=" OR p.total LIKE '".$requestData['search']['value']."%' )";
+if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
+$sql = "SELECT p.id,p.no_faktur,p.total,p.suplier,p.tanggal,p.tanggal_jt,p.jam,p.user,p.status,p.potongan,p.tax,p.sisa,p.kredit,p.nilai_kredit,s.nama,g.nama_gudang";
+$sql.=" FROM pembelian p INNER JOIN suplier s ON p.suplier = s.id INNER JOIN gudang g ON p.kode_gudang = g.kode_gudang ";
+$sql.=" WHERE p.tanggal <= '$dari_tanggal' AND p.tanggal <= '$sampai_tanggal'";
+$sql.=" AND kredit != 0";
+
+	$sql.=" AND ( no_faktur LIKE '".$requestData['search']['value']."%' ";  
+	$sql.=" OR tanggal LIKE '".$requestData['search']['value']."%' ";
+	$sql.=" OR tanggal_jt LIKE '".$requestData['search']['value']."%' ";   
+	$sql.=" OR total LIKE '".$requestData['search']['value']."%' ";   
+	$sql.=" OR nama LIKE '".$requestData['search']['value']."%' )";
 
 }
 
@@ -73,53 +77,50 @@ while( $row=mysqli_fetch_array($query) ) {
 
 	$nestedData=array(); 
 
-	$nestedData[] = $row["penjamin"];
 	$nestedData[] = $row["tanggal"];
 	$nestedData[] = $row["tanggal_jt"];
 	$nestedData[] = $row["no_faktur"];
-	if ($row["nama_pelanggan"] == "") {		
-	$nestedData[] = "Umum";
-	}
-	else{
-	$nestedData[] = $row["nama_pelanggan"];
-	}
+	$nestedData[] = $row["nama"];
 	$nestedData[] = rp($row["total"]);
 	$nestedData[] = $row["jam"];
 	$nestedData[] = $row["user"];
 	$nestedData[] = $row["status"];
 	$nestedData[] = $row["potongan"];
 	$nestedData[] = $row["tax"];
+	$nestedData[] = rp($row["sisa"]);
 	$nestedData[] = rp($row["kredit"]);
-	$nestedData[] = rp($row["tunai"]);
-	$nestedData[] = rp($row["id"]);
-	
+	$nestedData[] = rp($row["nilai_kredit"]);
+	$nestedData[] = $row["id"];
 	
 	$data[] = $nestedData;
 		
 }
 
+$query300 = $db->query("SELECT SUM(nilai_kredit) AS total_nilai_kredit FROM pembelian WHERE  tanggal <= '$dari_tanggal' AND tanggal <= '$sampai_tanggal' AND kredit != 0");
+$cek300 = mysqli_fetch_array($query300);
+$total_nilai_kredit = $cek300['total_nilai_kredit'];
 
-$query02 = $db->query("SELECT SUM(total) AS total_akhir FROM penjualan WHERE tanggal <= '$sampai_tanggal' AND kredit != 0");
-$cek02 = mysqli_fetch_array($query02);
-$total_akhir = $cek02['total_akhir'];
 
-$query01 = $db->query("SELECT SUM(potongan) AS total_potongan FROM penjualan WHERE tanggal <= '$sampai_tanggal' AND kredit != 0");
-$cek01 = mysqli_fetch_array($query01);
-$total_potongan = $cek01['total_potongan'];
-
-$query20 = $db->query("SELECT SUM(tax) AS total_tax FROM penjualan WHERE tanggal <= '$sampai_tanggal' AND kredit != 0");
-$cek20 = mysqli_fetch_array($query20);
-$total_tax = $cek20['total_tax'];
-
-$query30 = $db->query("SELECT SUM(kredit) AS total_kredit FROM penjualan WHERE tanggal <= '$sampai_tanggal' AND kredit != 0");
+$query30 = $db->query("SELECT SUM(kredit) AS total_kredit FROM pembelian WHERE tanggal <= '$dari_tanggal' AND tanggal <= '$sampai_tanggal' AND kredit != 0");
 $cek30 = mysqli_fetch_array($query30);
 $total_kredit = $cek30['total_kredit'];
 
 
-$nestedData = array();
+$query02 = $db->query("SELECT SUM(total) AS total_akhir FROM pembelian WHERE tanggal <= '$dari_tanggal' AND tanggal <= '$sampai_tanggal' AND kredit != 0");
+$cek02 = mysqli_fetch_array($query02);
+$total_akhir = $cek02['total_akhir'];
+
+$query01 = $db->query("SELECT SUM(potongan) AS total_potongan FROM pembelian WHERE tanggal <= '$dari_tanggal' AND tanggal <= '$sampai_tanggal' AND kredit != 0");
+$cek01 = mysqli_fetch_array($query01);
+$total_potongan = $cek01['total_potongan'];
+
+$query20 = $db->query("SELECT SUM(tax) AS total_tax FROM pembelian WHERE tanggal <= '$dari_tanggal' AND tanggal <= '$sampai_tanggal' AND kredit != 0");
+$cek20 = mysqli_fetch_array($query20);
+$total_tax = $cek20['total_tax'];
+		
+	$nestedData = array();
 
 	$nestedData[] = "<b style='color:red' >Total :</b>";
-	$nestedData[] = "";
 	$nestedData[] = "";
 	$nestedData[] = "";
 	$nestedData[] = "";
@@ -129,15 +130,15 @@ $nestedData = array();
 	$nestedData[] = "";
 	$nestedData[] = "<b style='color:red'>". rp($total_potongan) ."</b>";
 	$nestedData[] = "<b style='color:red'>". rp($total_tax) ."</b>";
-	$nestedData[] = "<b style='color:red'>". rp($total_kredit) ."</b>";
 	$nestedData[] = "";
+	$nestedData[] = "<b style='color:red'>". rp($total_kredit) ."</b>";
+	$nestedData[] = "<b style='color:red'>". rp($total_nilai_kredit) ."</b>";
 	$nestedData[] = "";
 	$nestedData[] = "";
 	$nestedData[] = "";
 	$nestedData[] = "";
 	$nestedData[] = "";
 $data[] = $nestedData;
-
 
 
 $json_data = array(
