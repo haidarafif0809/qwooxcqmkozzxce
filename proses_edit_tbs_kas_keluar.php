36c7e9ecdaa8c10e624c882e3a1779eabc6ecdc9
Rikopernando <rikopernando96@gmail.com>
2017-02-08 10:42:11 +0700

    perbaikan Kas masuk dan kas keluar
    perbaikan Kas masuk dan kas keluar,  tanggal dan jam diambil dari form

--- proses_edit_tbs_kas_keluar.php
+++ proses_edit_tbs_kas_keluar.php
@@ -8,11 +8,11 @@
     //mengirim data sesuai dengan variabel denagn metode POST 
 
 // buat prepared statements
-    $stmt = $db->prepare("INSERT INTO tbs_kas_keluar (no_faktur,keterangan,dari_akun,ke_akun,jumlah,tanggal,jam,user) VALUES (?,?,?,?,?,now(),now(),?)");
+    $stmt = $db->prepare("INSERT INTO tbs_kas_keluar (no_faktur,keterangan,dari_akun,ke_akun,jumlah,tanggal,jam,user) VALUES (?,?,?,?,?,?,?,?)");
 
 // hubungkan "data" dengan prepared statements
-        $stmt->bind_param("ssssis", 
-        $no_faktur, $keterangan, $dari_akun, $ke_akun, $jumlah, $user);        
+        $stmt->bind_param("ssssisss", 
+        $no_faktur, $keterangan, $dari_akun, $ke_akun, $jumlah,$tanggal,$jam, $user);        
         
 // siapkan "data" query
         $no_faktur = stringdoang($_POST['no_faktur']);
@@ -21,6 +21,8 @@
         $ke_akun = stringdoang($_POST['ke_akun']);
         $jumlah = angkadoang($_POST['jumlah']);
         $user = $_SESSION['user_name'];
+        $tanggal = stringdoang($_POST['tanggal']);
+         $jam = stringdoang($_POST['jam']);
 
 // jalankan query
         $stmt->execute();
