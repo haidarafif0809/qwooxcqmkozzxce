f53e4917c7ec0a1a65e3a4cc9023d3c28ba4b6af
RindangRamadhan <rindangramadhan10@gmail.com>
2017-02-11 17:40:57 +0700

    Update No Faktur Penjualan
    Query u/ update no faktur, jika terjadi no faktur yg doble

--- query_update_no_fak.php
+++ query_update_no_fak.php
@@ -1,49 +1,28 @@
 <?php 
 include 'db.php';
 
-$no_faktur_terakhir_bener = 4345;
+$no_faktur_terakhir_bener = 1803;
 
 
-$gantis = $db->query("SELECT * FROM penjualan WHERE tanggal = '2017-01-27'  AND jam < '21:00:00' ");
+$gantis = $db->query("SELECT * FROM penjualan WHERE id >= '9049' AND id <= '9091' ");
 while($ganti = mysqli_fetch_array($gantis))
 {
  
 $no_faktur_terakhir_bener = $no_faktur_terakhir_bener + 1;
-$no_faktur_terakhir_bener = $no_faktur_terakhir_bener."/JL/01/17";
+$no_faktur_terakhir_bener = $no_faktur_terakhir_bener."/JL/02/17";
 
 
-$update = $db->query("UPDATE penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'  ");
 
 $update_detail = $db->query("UPDATE detail_penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'  ");
 
 $update_fee = $db->query("UPDATE laporan_fee_produk SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'  ");
 
-$waktu = $ganti['tanggal']." ".$ganti['jam'];
+$hpp_keluar = $db->query("UPDATE hpp_keluar SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]' ");
 
-
-
-$hpp_keluar = $db->query("UPDATE hpp_keluar SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' 
-	AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]' ");
-
-
-
-echo "UPDATE hpp_keluar SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' 
-	AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'";
-	echo "<br>";
-
-
-	echo "UPDATE laporan_fee_produk SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'";
-	echo "<br>"; 
-
-echo "UPDATE detail_penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'";
-echo "<br>"; 
-
-
-echo "UPDATE penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'";
-echo "<br>";
+$update = $db->query("UPDATE penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]' ");
 
 } 
 
+echo "SUKES";
 
-
- ?>
+?>
