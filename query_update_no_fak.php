8a7ecbc7f592274092a607d7092f8cce011bff30
RindangRamadhan <rindangramadhan10@gmail.com>
2017-02-11 18:00:18 +0700

    Update No faktur Penjualan
    update penjualan ulang

--- query_update_no_fak.php
+++ query_update_no_fak.php
@@ -12,13 +12,6 @@ $no_faktur_terakhir_bener = $no_faktur_terakhir_bener + 1;
 $no_faktur_terakhir_bener = $no_faktur_terakhir_bener."/JL/02/17";
 
 
-
-$update_detail = $db->query("UPDATE detail_penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'  ");
-
-$update_fee = $db->query("UPDATE laporan_fee_produk SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'  ");
-
-$hpp_keluar = $db->query("UPDATE hpp_keluar SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]' ");
-
 $update = $db->query("UPDATE penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]' ");
 
 } 
