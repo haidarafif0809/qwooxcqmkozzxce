2a90e1bb23d9857cb490dfba607f90367358d985
Rikopernando <rikopernando96@gmail.com>
2017-02-15 17:32:56 +0700

    perbaikan form item keluar
    perbaikan form item keluar=> select kode barang nya diganti menggunakan
    chace

--- form_item_keluar.php
+++ form_item_keluar.php
@@ -14,7 +14,9 @@ $session_id = session_id();
 
 
  ?>
-
+<!-- js untuk tombol shortcut -->
+ <script src="shortcut.js"></script>
+<!-- js untuk tombol shortcut -->
 <!--membuat tampilan form agar terlihat rapih dalam satu tempat-->
 <div class="container">
 
@@ -35,11 +37,7 @@ $session_id = session_id();
           <input type="hidden" name="session_id" id="session_id" class="form-control" readonly="" value="<?php echo $session_id; ?>" required="" >
           
          </form>
-   
-
-<!-- membuat tombol agar menampilkan modal -->
-<button type="button" class="btn btn-info" id="cari_item_keluar1" data-toggle="modal" data-target="#myModal"> <i class='fa fa-search'> </i> Cari</button>
-<br>
+  
 <!-- Tampilan Modal -->
 <div id="myModal" class="modal fade" role="dialog">
   <div class="modal-dialog ">
@@ -81,23 +79,39 @@ $session_id = session_id();
 </div>
 
 
-      <div class="col-sm-9">
+      <div class="col-sm-8">
+
+      <!-- membuat tombol agar menampilkan modal -->
+<button type="button" class="btn btn-info" id="cari_item_keluar1" data-toggle="modal" data-target="#myModal"> <i class='fa fa-search'> </i> Cari</button>
+<br>
+
 <!-- membuat form -->
- <form class="form-inline" action="proses_tbs_item_keluar.php" role="form" id="formtambahproduk">
+ <form class="form-control" action="proses_tbs_item_keluar.php" role="form" id="formtambahproduk">
   
 
   <!-- agar tampilan berada pada satu group -->
   <!-- memasukan teks pada kolom kode barang -->
-  <div class="col-sm-3"> 
-    <input style="height: 20px" type="text" class="form-control"  name="kode_barang" id="kode_barang" autocomplete="off" placeholder="Ketikkan Kode Produk">
-  </div>
+  <div class="col-sm-4"> 
 
- <div class="col-sm-1">
+    <select style="font-size:15px; height:20px" type="text" name="kode_barang" id="kode_barang" class="form-control chosen" data-placeholder="SILAKAN PILIH...">
+    <option value="">SILAKAN PILIH...</option>
+       <?php 
+
+        include 'cache.class.php';
+          $c = new Cache();
+          $c->setCache('produk');
+          $data_c = $c->retrieveAll();
+
+          foreach ($data_c as $key) {
+            echo '<option id="opt-produk-'.$key['kode_barang'].'" value="'.$key['kode_barang'].'" data-kode="'.$key['kode_barang'].'" nama-barang="'.$key['nama_barang'].'" satuan="'.$key['satuan'].'" kategori="'.$key['kategori'].'" status="'.$key['status'].'" suplier="'.$key['suplier'].'" limit_stok="'.$key['limit_stok'].'" ber-stok="'.$key['berkaitan_dgn_stok'].'" tipe_barang="'.$key['tipe_barang'].'" id-barang="'.$key['id'].'" > '. $key['kode_barang'].' ( '.$key['nama_barang'].' ) </option>';
+          }
+
+        ?>
+    </select>
+
   <input type="hidden" class="form-control" name="nama_barang" id="nama_barang" readonly="" >
-</div>
-
-
-  <div class="col-sm-2">
+  </div>
+<div class="col-sm-2">
     <input style="height: 20px; width: 70px" type="text" class="form-control" name="jumlah_barang" id="jumlah_barang" autocomplete="off" placeholder="Jumlah " onkeydown="return numbersonly(this, event);" onkeyup="javascript:tandaPemisahTitik(this);">
   </div>
 
@@ -105,10 +119,11 @@ $session_id = session_id();
  <input type="hidden" id="jml_barang" name="jml_barang" class="form-control">
 <!-- memasukan teks pada kolom satuan, harga, dan nomor faktur namun disembunyikan -->
   <input type="hidden" id="satuan_produk" name="satuan" class="form-control" value="" required="">
-  <input type="hidden" id="harga_produk" name="harga" class="form-control" value="" required="">
+  <input type="hidden" id="harga_produk" name="harga" class="form-control" value="" required="" placeholder="harga produk">
   <input type="hidden" name="session_id" id="session_id" class="form-control" value="<?php echo $session_id; ?>" required="" >
   
   <!-- membuat tombol submit-->
+
   <button type="submit" id="submit_produk" class="btn btn-success"> <i class='fa fa-plus'> </i> Tambah Produk</button>
 </form>
 
@@ -135,7 +150,7 @@ $session_id = session_id();
     <?php
 
     //menampilkan semua data yang ada pada tabel tbs penjualan dalam DB
-     $perintah = $db->query("SELECT tik.id,tik.no_faktur,tik.kode_barang,tik.nama_barang,tik.jumlah,tik.harga,tik.subtotal,s.nama FROM tbs_item_keluar tik INNER JOIN satuan s ON tik.satuan = s.id WHERE session_id = '$session_id'");
+     $perintah = $db->query("SELECT tik.id,tik.no_faktur,tik.kode_barang,tik.nama_barang,tik.jumlah,tik.harga,tik.subtotal,s.nama FROM tbs_item_keluar tik LEFT JOIN satuan s ON tik.satuan = s.id WHERE tik.session_id = '$session_id'");
 
       //menyimpan data sementara yang ada pada $perintah
 
@@ -167,7 +182,8 @@ mysqli_close($db);
   </div>
         </span>
                   
-
+                <h6 style="text-align: left ; color: red"><i> * Klik 2x pada kolom jumlah barang jika ingin mengedit.</i></h6>
+                <h6 style="text-align: left ;"><i><b> * Short Key (F2) untuk mencari Kode Produk atau Nama Produk.</b></i></h6>
   </div><!-- end of col sm 8 --> <!--tag penutup col sm 8-->
 
  <div class="col-sm-3" id="col_sm_4"> <!--tag pembuka col sm 4-->
@@ -181,9 +197,9 @@ mysqli_close($db);
       <label>Keterangan </label><br>
       <textarea name="keterangan" style="height: 80px" id="keterangan" class="form-control"></textarea>
 
-<input type="hidden" name="session_id" id="nomorfaktur" class="form-control" value="<?php echo $session_id; ?>">
+      <input type="hidden" name="session_id" id="nomorfaktur" class="form-control" value="<?php echo $session_id; ?>">
 
-<a class="btn btn-primary" href="form_item_keluar.php" id="transaksi_baru" style="display: none"> <i class='fa fa-refresh'> </i> Transaksi Baru</a>
+        <a class="btn btn-primary" href="form_item_keluar.php" id="transaksi_baru" style="display: none"> <i class='fa fa-refresh'> </i> Transaksi Baru</a>
   <!--membuat tombol submit bayar & Hutang-->
       <button type="submit" id="pembayaran_item_keluar" class="btn btn-info"><i class='fa fa-send'> </i> Selesai </a> </button>
       
@@ -292,6 +308,21 @@ mysqli_close($db);
 
 </div><!-- end of container -->
 
+<script>
+//untuk menampilkan data tabel
+$(document).ready(function(){
+    $("#kode_barang").trigger('chosen:open');
+
+});
+
+</script>
+
+
+      <script type="text/javascript">
+      
+      $(".chosen").chosen({no_results_text: "Maaf, Data Tidak Ada!",search_contains:true});  
+      
+      </script>
 
                               
 <script>
@@ -300,7 +331,17 @@ $(document).ready(function() {
         $('#tableuser').DataTable({"ordering":false});
     });
 
-</script>                 
+</script>   
+
+<script> 
+    shortcut.add("f2", function() {
+        // Do something
+
+        $("#kode_barang").trigger('chosen:open');
+
+
+    });   
+  </script>              
           
 
 <!--untuk memasukkan perintah java script-->
@@ -308,7 +349,14 @@ $(document).ready(function() {
 
 // jika dipilih, nim akan masuk ke input dan modal di tutup
   $(document).on('click', '.pilih', function (e) {
+
+
+
   document.getElementById("kode_barang").value = $(this).attr('data-kode');
+
+  $("#kode_barang").trigger('chosen:updated');
+
+
   document.getElementById("nama_barang").value = $(this).attr('nama-barang');
   document.getElementById("satuan_produk").value = $(this).attr('satuan');
   document.getElementById("harga_produk").value = $(this).attr('harga');
@@ -328,20 +376,12 @@ $(document).ready(function() {
 
   </script>
 
-<script>
-$(function() {
-    $( "#kode_barang" ).autocomplete({
-        source: 'kode_barang_autocomplete.php'
-    });
-});
-</script>
 
    <script>
    //untuk menampilkan data yang diambil pada form tbs penjualan berdasarkan id=formtambahproduk
   $("#submit_produk").click(function(){
 
     var kode_barang = $("#kode_barang").val();
-          var kode_barang = kode_barang.substr(0, kode_barang.indexOf('('));
     var nama_barang = $("#nama_barang").val();
     var jumlah_barang = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#jumlah_barang").val()))));
     var jml_barang = $("#jml_barang").val();
@@ -361,34 +401,38 @@ $(function() {
                                           
     var sub_tbs = parseInt(harga,10) * parseInt(jumlah_barang,10);
     var subtotal = parseInt(total,10) + parseInt(sub_tbs,10);
-                                          
-                                          
-
-     $("#kode_barang").val('');
-     $("#nama_barang").val('');
-     $("#jumlah_barang").val('');               
+                                                           
                                     
       if (jumlah_barang == ""){
       alert("Jumlah Barang Harus Diisi");
+      $("#jumlah_barang").focus();   
       }
       else if (kode_barang == ""){
       alert("Kode Harus Diisi");
+
+
+    $("#kode_barang").trigger('chosen:open');
+
+
       }
-      else if ( stok < 0){
+      else if (stok < 0){
       
       alert ("Jumlah Melebihi Stok!");
+      $("#jumlah_barang").val('');
+      $("#jumlah_barang").focus();
       
       }
       
       else
       {
 
-        $("#total_item_keluar").val(tandaPemisahTitik(subtotal));     
- $.post("proses_tbs_item_keluar.php",{session_id:session_id,kode_barang:kode_barang,jumlah_barang:jumlah_barang,satuan:satuan,nama_barang:nama_barang,harga:harga},function(info) {
+        $("#total_item_keluar").val(tandaPemisahTitik(subtotal));    
+
+     $.post("proses_tbs_item_keluar.php",{session_id:session_id,kode_barang:kode_barang,jumlah_barang:jumlah_barang,satuan:satuan,nama_barang:nama_barang,harga:harga},function(info) {
 
      $("#tbody").prepend(info);
-     $("#kode_barang").val('');
-     $("#nama_barang").val('');
+     $("#kode_barang").val('').trigger("chosen:updated");
+     $("#kode_barang").trigger('chosen:open');
      $("#jumlah_barang").val('');
        
        });
@@ -403,20 +447,20 @@ $(function() {
 
   });
 
-//menampilkan no urut faktur setelah tombol click di pilih
+/*/menampilkan no urut faktur setelah tombol click di pilih
    $("#cari_item_keluar1").click(function() {
      $.get('no_faktur_IK.php', function(data) {
-   /*optional stuff to do after getScript */ 
+   /*optional stuff to do after getScript 
 $("#nomorfaktur").val(data);
 $("#nomorfaktur1").val(data);
 
  });
 //menyembunyikan notif berhasil
      $("#alert_berhasil").hide();
-    /* Act on the event */
+    /* Act on the event 
   
 
-  });
+  });*/
 
    </script>
 
@@ -441,7 +485,7 @@ $("#nomorfaktur1").val(data);
           "fnCreatedRow": function( nRow, aData, iDataIndex ) {
 
               $(nRow).attr('class', "pilih");
-              $(nRow).attr('data-kode', aData[0]+"("+aData[1]+")");
+              $(nRow).attr('data-kode', aData[0]);
               $(nRow).attr('nama-barang', aData[1]);
               $(nRow).attr('satuan', aData[7]);
               $(nRow).attr('harga', aData[6]);
@@ -561,6 +605,9 @@ $.post("cek_total_item_keluar.php",
             if (data != "") {
             
             $(".tr-id-"+id).remove();
+
+            $("#kode_barang").val('').trigger("chosen:updated");
+             $("#kode_barang").trigger('chosen:open');
             
             }
             });
@@ -578,7 +625,7 @@ $.post("cek_total_item_keluar.php",
    <script type="text/javascript">
      
    $("#jumlah_barang").keyup(function(){
-    var jumlah_barang = $("#jumlah_barang").val();
+    var jumlah_barang = bersihPemisah(bersihPemisah(bersihPemisah(bersihPemisah($("#jumlah_barang").val()))));
     var jml_barang = $("#jml_barang").val();
     var stok = jml_barang - jumlah_barang;
 
@@ -593,7 +640,7 @@ $.post("cek_total_item_keluar.php",
 
    </script>
 
-<script type="text/javascript">
+<!--script type="text/javascript">
   
         $(document).ready(function(){
         $("#kode_barang").blur(function(){
@@ -641,7 +688,7 @@ $.post("cek_total_item_keluar.php",
       
       
 </script>
-
+-->
                               <script type="text/javascript">
                                  
                                  $(".edit-jumlah").dblclick(function(){
@@ -719,6 +766,8 @@ $.post("cek_total_item_keluar.php",
   if(data == 1){
     alert("Anda Tidak Bisa Menambahkan Barang Yang Sudah Ada, Silakan Edit atau Pilih Barang Yang Lain !");
     $("#kode_barang").val('');
+    $("#kode_barang").trigger('chosen:updated');
+    $("#kode_barang").trigger('chosen:open');
     $("#nama_barang").val('');
    }//penutup if
 
@@ -728,6 +777,72 @@ $.post("cek_total_item_keluar.php",
   });//penutup ready(function()
 </script>   
 
+
+
+<script type="text/javascript">
+  
+  $(document).ready(function(){
+  $("#kode_barang").change(function(){
+
+    var session_id = $("#session_id").val();
+    var kode_barang = $(this).val();
+    var nama_barang = $('#opt-produk-'+kode_barang).attr("nama-barang");
+    var harga_hpp = $('#opt-produk-'+kode_barang).attr("harga");
+    var jumlah_barang = $('#opt-produk-'+kode_barang).attr("jumlah-barang");
+    var satuan = $('#opt-produk-'+kode_barang).attr("satuan");
+    var kategori = $('#opt-produk-'+kode_barang).attr("kategori");
+    var status = $('#opt-produk-'+kode_barang).attr("status");
+    var suplier = $('#opt-produk-'+kode_barang).attr("suplier");
+    var limit_stok = $('#opt-produk-'+kode_barang).attr("limit_stok");
+    var ber_stok = $('#opt-produk-'+kode_barang).attr("ber-stok");
+    var tipe_produk = $('#opt-produk-'+kode_barang).attr("tipe_barang");
+    var id_barang = $('#opt-produk-'+kode_barang).attr("id-barang");
+
+
+    $("#kode_barang").val(kode_barang);
+    $("#nama_barang").val(nama_barang);
+    $("#satuan_produk").val(satuan);
+
+
+    $.post("ambil_harga_hpp.php",{kode_barang:kode_barang},function(data){
+        $("#harga_produk").val(data);
+    });
+
+   
+
+if (ber_stok == 'Barang') {
+
+    $.post('ambil_jumlah_produk.php',{kode_barang:kode_barang}, function(data){
+      if (data == "") {
+        data = 0;
+      }
+      $("#jml_barang").val(data);
+    });
+
+}
+
+
+  $.post('cek_kode_barang_tbs_item_keluar.php',{kode_barang:kode_barang,session_id:session_id}, function(data){
+            
+            if(data == 1){
+                        alert("Anda Tidak Bisa Menambahkan Barang Yang Sudah Ada, Silakan Edit atau Pilih Barang Yang Lain !");
+
+                        $("#kode_barang").val('').trigger("chosen:updated");
+                        $("#kode_barang").trigger('chosen:open');
+                        $(".chosen").chosen({no_results_text: "Maaf, Data Tidak Ada!",search_contains:true}); 
+                }//penutup if     
+
+
+
+                });
+
+
+      });
+  });
+
+      
+      
+</script>
 
 <!-- memasukan file footer.php -->
 <?php include 'footer.php'; ?>
