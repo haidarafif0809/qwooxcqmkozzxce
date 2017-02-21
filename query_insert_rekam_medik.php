9f60a9725f124499f3a91256847219c6096b9d72
RindangRamadhan <rindangramadhan10@gmail.com>
2017-02-21 14:19:13 +0700

    Query u/ Insert Rekam Medik (Tugu)

---
+++ query_insert_rekam_medik.php
@@ -0,0 +1,25 @@
+<?php 
+include 'db.php';
+include_once 'sanitasi.php';
+
+$select_reg = $db->query("SELECT * FROM reg  ");
+while ($dp = mysqli_fetch_array($select_reg)) {
+
+	$select = $db->query("SELECT * FROM registrasi WHERE no_reg = '$dp[no_reg]'");
+	$ambil = mysqli_fetch_array($select);
+
+	$rekam_medik = $db->query("INSERT INTO rekam_medik(alergi, no_kk, nama_kk, no_reg, no_rm, nama, alamat, umur, jenis_kelamin, sistole_distole, suhu, berat_badan, tinggi_badan, nadi, respiratory, poli, tanggal_periksa, jam, dokter, kondisi, rujukan) VALUES ('$ambil[alergi]', '$ambil[no_kk]', '$ambil[nama_kk]', '$ambil[no_reg]', '$ambil[no_rm]', '$ambil[nama_pasien]', '$ambil[alamat_pasien]', '$ambil[umur_pasien]', '$ambil[jenis_kelamin]', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', '$ambil[poli]', '$ambil[tanggal] $ambil[jam]', '$ambil[jam]', '$ambil[dokter]', '$ambil[kondisi]', '$ambil[rujukan]')";
+
+
+	if ($db->query($rekam_medik) === TRUE) {
+
+	} 
+    else{
+	echo "Error: " . $rekam_medik . "<br>" . $db->error;
+	}
+}
+
+	echo "SUKSES";
+//Untuk Memutuskan Koneksi Ke Database
+mysqli_close($db);
+?>
