7f47a589a281be082e4e6966024207487c69fc0f
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-02-22 13:56:17 +0700

    perbaikan form penjualan inap,jalan,ugd
    perbaikan pengambilan tbs saat submit produk (barang tidak muncul & subtotal muncul)

--- cetak_penjualan_tunai_besar_ranap.php
+++ cetak_penjualan_tunai_besar_ranap.php
@@ -194,9 +194,9 @@ include 'db.php';
         </tbody>
     </table>
 
-<h6 align="right"><b>Subtotal Bed : <?php echo rp($subtotal_bed); ?></b></h6>
+<h6 align="right"><b>Subtotal Kamar : <?php echo rp($subtotal_bed); ?></b></h6>
 
-<h6><b>Jasa</b></h6>
+<h6><b>Jasa & Tindakan </b></h6>
 
 <table id="tableuser" class="table table-bordered table-sm">
         <thead>
@@ -257,9 +257,9 @@ include 'db.php';
    </tbody>
 </table>
 
-<h6 align="right"><b>Subtotal Jasa : <?php echo rp($subtotal_jasa); ?></b></h6>
+<h6 align="right"><b>Subtotal Jasa & Tindakan : <?php echo rp($subtotal_jasa); ?></b></h6>
 
-<h6><b>Barang / Obat Obatan </b></h6>
+<h6><b> Obat Obatan / Alkes </b></h6>
 
 <table id="tableuser" class="table table-bordered table-sm">
         <thead>
@@ -318,7 +318,7 @@ include 'db.php';
    </tbody>
 </table>
 
-<h6 align="right"><b>Subtotal Barang / Obat Obatan : <?php echo rp($subtotal_barang); ?></b></h6>
+<h6 align="right"><b>Subtotal Obat Obatan / Alkes : <?php echo rp($subtotal_barang); ?></b></h6>
 
 <h6><b>Laboratorium</b></h6>
 
