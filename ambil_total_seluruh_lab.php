888f5385138403afc1b4581877d93c4ea24263aa
RindangRamadhan <rindangramadhan10@gmail.com>
2017-02-24 15:29:22 +0700

    Lap. Penj. Gol. Produk
    Mengubah cara perhitungan total harga produk(jasa)

--- ambil_total_seluruh_lab.php
+++ ambil_total_seluruh_lab.php
@@ -6,7 +6,7 @@ $sampai_tanggal = stringdoang($_GET['sampai_tanggal']);
 $dari_tanggal = stringdoang($_GET['dari_tanggal']);
 
 
-$query = $db->query("SELECT no_faktur, potongan, biaya_admin FROM penjualan WHERE tanggal >= '$dari_tanggal'  AND tanggal <= '$sampai_tanggal'	");
+$query = $db->query("SELECT no_faktur, potongan, biaya_admin, no_reg FROM penjualan WHERE tanggal >= '$dari_tanggal'  AND tanggal <= '$sampai_tanggal'	");
 $total_row = 0;
 while ($data = $query->fetch_array()) {
 
@@ -14,10 +14,16 @@ while ($data = $query->fetch_array()) {
 	$row = mysqli_fetch_array($select);
 
 	$select_kamar = $db->query("SELECT SUM(dp.subtotal) AS total_kamar FROM detail_penjualan dp INNER JOIN bed b ON dp.kode_barang = b.nama_kamar WHERE dp.no_faktur = '$data[no_faktur]' ");
-	$data_kamar = mysqli_fetch_array($select);
+	$data_kamar = mysqli_fetch_array($select_kamar);
 	$sub_kamar = $data_kamar['total_kamar'];
 
-	$sub_bener = ($row['total'] + $data['biaya_admin'] - $data['potongan']) + $sub_kamar;
+	$select_operasi = $db->query("SELECT SUM(b.harga_jual) AS total_operasi FROM hasil_operasi b INNER JOIN penjualan dp ON b.no_reg = dp.no_reg WHERE b.no_reg = '$data[no_reg]' ");
+	$data_operasi = mysqli_fetch_array($select_operasi);
+	$sub_operasi = $data_operasi['total_operasi'];
+
+	$sub_detail = $row['total'] + $data['biaya_admin'] - $data['potongan'];
+
+	$sub_bener = $sub_detail + $sub_kamar + $sub_operasi;
 	$total_row = $total_row + $sub_bener;
 
 }
