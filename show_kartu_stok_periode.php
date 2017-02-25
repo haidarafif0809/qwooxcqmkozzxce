33b8d303723f69b2bb6c666c3df28d9b39b65b56
RindangRamadhan <rindangramadhan10@gmail.com>
2017-02-25 14:58:54 +0700

    Kartu Stok
    Hitung saldo awal itu <= dari tanggal, bukan >= dari tanggal

--- show_kartu_stok_periode.php
+++ show_kartu_stok_periode.php
@@ -24,7 +24,7 @@ if ($requestData['start'] > 0)
 {	
 
 		// getting total number records without any search
-		$sql = $db->query("SELECT no_faktur,jumlah_kuantitas,jenis_transaksi,tanggal,jenis_hpp,waktu, id FROM hpp_masuk WHERE kode_barang = '$kode_barang' AND tanggal >= '$dari_tanggal' UNION SELECT no_faktur, jumlah_kuantitas,jenis_transaksi, tanggal, jenis_hpp,waktu, id FROM hpp_keluar WHERE kode_barang = '$kode_barang' AND tanggal >= '$dari_tanggal' ORDER BY tanggal  LIMIT ".$requestData['start']."  ");
+		$sql = $db->query("SELECT no_faktur,jumlah_kuantitas,jenis_transaksi,tanggal,jenis_hpp,waktu, id FROM hpp_masuk WHERE kode_barang = '$kode_barang' AND tanggal <= '$dari_tanggal' UNION SELECT no_faktur, jumlah_kuantitas,jenis_transaksi, tanggal, jenis_hpp,waktu, id FROM hpp_keluar WHERE kode_barang = '$kode_barang' AND tanggal >= '$dari_tanggal' ORDER BY tanggal  LIMIT ".$requestData['start']."  ");
 		while($row = mysqli_fetch_array($sql))
 		{
 					if ($row['jenis_hpp'] == '1')
