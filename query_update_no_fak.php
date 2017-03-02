025e3d9dba0f2b0350ebaaa6813b68bcc04427c9
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-03-02 13:48:40 +0700

    query update no_ faktur ulang
    query update no_ faktur ulang

--- query_update_no_fak.php
+++ query_update_no_fak.php
@@ -1,21 +1,45 @@
-<?php 
-include 'db.php';
-
-$no_faktur_terakhir_bener = 1803;
-
-
-$gantis = $db->query("SELECT * FROM penjualan WHERE id >= '9049' AND id <= '9092' ");
-while($ganti = mysqli_fetch_array($gantis))
-{
+<?php  
+include 'db.php'; 
  
-$no_faktur_terakhir_bener = $no_faktur_terakhir_bener + 1;
-$no_faktur_terakhir_bener = $no_faktur_terakhir_bener."/JL/02/17";
+$no_faktur_terakhir_bener = 1; 
+ 
+ 
+$gantis = $db->query("SELECT * FROM penjualan WHERE id >= '638' AND id <= '656' "); 
+while($ganti = mysqli_fetch_array($gantis)) 
+{ 
+  
+$no_faktur_terakhir_bener = $no_faktur_terakhir_bener + 1; 
+$no_faktur_terakhir_bener = $no_faktur_terakhir_bener."/JL/03/17"; 
+ 
+ 
+$update = $db->query("UPDATE penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE  tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'  "); 
+ 
+if ($ganti['jenis_penjualan'] == 'Rawat Inap' )
+{
+
+$update_detail = $db->query("UPDATE detail_penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE  no_reg = '$ganti[no_reg]'  "); 
+
+$update_fee = $db->query("UPDATE laporan_fee_produk SET no_faktur = '$no_faktur_terakhir_bener' WHERE  no_reg = '$ganti[no_reg]'  "); 
 
 
-$update = $db->query("UPDATE penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE no_faktur = '$ganti[no_faktur]' AND tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]' ");
 
-} 
+ }
+ else
+ {
 
-echo "SUKES";
 
-?>
+$update_detail = $db->query("UPDATE detail_penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE  tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'   "); 
+
+$update_fee = $db->query("UPDATE laporan_fee_produk SET no_faktur = '$no_faktur_terakhir_bener' WHERE  tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'   "); 
+ 
+
+
+ }
+  
+ $hpp_keluar = $db->query("UPDATE hpp_keluar SET no_faktur = '$no_faktur_terakhir_bener' WHERE  tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]' "); 
+ 
+}  
+ 
+ 
+ echo "sukses";
+ ?>
