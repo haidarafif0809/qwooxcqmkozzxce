ad97363b9ec9b04934bfc27e355fc3d26da21897
RindangRamadhan <rindangramadhan10@gmail.com>
2017-03-21 11:11:07 +0700

    File Untuk Menghitung Subtotal Tbs
    - Menambahkan mysqli_num_rows untuk diajdikan logika If

--- cek_jumlah_disc_dg_total_sub_tbs.php
+++ cek_jumlah_disc_dg_total_sub_tbs.php
@@ -4,9 +4,11 @@ include 'sanitasi.php';
 include 'db.php';
 	$session_id = session_id();
 
- $querytbs = $db->query("SELECT sum(subtotal) as totale , sum(potongan) as potongane FROM tbs_penjualan WHERE session_id = '$session_id'");
+$querytbs = $db->query("SELECT sum(subtotal) as totale , sum(potongan) as potongane FROM tbs_penjualan WHERE session_id = '$session_id'");
+$row_tbs = mysqli_num_rows($querytbs);
 $idtbs = mysqli_fetch_array($querytbs);
-if ($idtbs > 0){
+
+if ($row_tbs > 0){
 
   echo json_encode($idtbs);
 }
