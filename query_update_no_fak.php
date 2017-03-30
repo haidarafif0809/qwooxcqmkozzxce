6fc35393710a70519f5f4e03288c08008af137e4
RindangRamadhan <rindangramadhan10@gmail.com>
2017-03-30 20:35:44 +0700

    Revert "Query Update No Faktur (Gemart)"
    This reverts commit 5403dcb046184daf363daef01854dbe9dea5c3e7.

--- query_update_no_fak.php
+++ query_update_no_fak.php
@@ -1,10 +1,10 @@
 <?php  
 include 'db.php'; 
  
-$no_faktur_terakhir_bener = 1004; 
+$no_faktur_terakhir_bener = 1; 
  
  
-$gantis = $db->query("SELECT * FROM penjualan WHERE id >= '5680' AND id <= '5704' "); 
+$gantis = $db->query("SELECT * FROM penjualan WHERE id >= '638' AND id <= '656' "); 
 while($ganti = mysqli_fetch_array($gantis)) 
 { 
   
@@ -14,15 +14,29 @@ $no_faktur_terakhir_bener = $no_faktur_terakhir_bener."/JL/03/17";
  
 $update = $db->query("UPDATE penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE  tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'  "); 
  
+if ($ganti['jenis_penjualan'] == 'Rawat Inap' )
+{
+
+$update_detail = $db->query("UPDATE detail_penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE  no_reg = '$ganti[no_reg]'  "); 
+
+$update_fee = $db->query("UPDATE laporan_fee_produk SET no_faktur = '$no_faktur_terakhir_bener' WHERE  no_reg = '$ganti[no_reg]'  "); 
+
+
+
+ }
+ else
+ {
+
 
 $update_detail = $db->query("UPDATE detail_penjualan SET no_faktur = '$no_faktur_terakhir_bener' WHERE  tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'   "); 
 
 $update_fee = $db->query("UPDATE laporan_fee_produk SET no_faktur = '$no_faktur_terakhir_bener' WHERE  tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]'   "); 
  
+
+
+ }
   
  $hpp_keluar = $db->query("UPDATE hpp_keluar SET no_faktur = '$no_faktur_terakhir_bener' WHERE  tanggal = '$ganti[tanggal]' AND jam = '$ganti[jam]' "); 
-  
- $hpp_keluar = $db->query("UPDATE jurnal_trans SET no_faktur = '$no_faktur_terakhir_bener' WHERE  DATE(waktu_jurnal) = '$ganti[tanggal]' AND TIME(waktu_jurnal) = '$ganti[jam]' "); 
  
 }  
  
