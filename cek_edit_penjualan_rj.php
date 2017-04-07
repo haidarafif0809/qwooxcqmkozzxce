48b627c1f9cb9504eebb8af840bf2ac876defd83
RindangRamadhan <rindangramadhan10@gmail.com>
2017-04-07 09:13:03 +0700

    Perbaikan Hasil Review Code
    Membenahi Select * pada file diatas

--- cek_edit_penjualan_rj.php
+++ cek_edit_penjualan_rj.php
@@ -26,7 +26,7 @@ $perintah2 = $db->query("DELETE FROM tbs_penjualan WHERE no_faktur = '$no_faktur
 
 
 //menampilkan seluruh data yang ada pada tabel pembelian dalan DB
-$perintah = $db->query("SELECT * FROM detail_penjualan WHERE no_faktur = '$no_faktur' AND no_reg = '$no_reg'");
+$perintah = $db->query("SELECT kode_barang, nama_barang, jumlah_barang, satuan, harga, subtotal, tipe_produk, tanggal, jam, potongan, tax, hpp, lab FROM detail_penjualan WHERE no_faktur = '$no_faktur' AND no_reg = '$no_reg'");
 while ($data = mysqli_fetch_array($perintah)){
 
 
@@ -73,7 +73,7 @@ while ($data_hasil = mysqli_fetch_array($query_hasil_pemeriksaan)){
 
 
 
-$perintah3 = $db->query("SELECT * FROM tbs_fee_produk WHERE  no_reg = '$no_reg' ");
+$perintah3 = $db->query("SELECT no_reg FROM tbs_fee_produk WHERE  no_reg = '$no_reg' ");
 $data1 = mysqli_num_rows($perintah3);
 
 if ($data1 > 0){
@@ -82,7 +82,7 @@ $perintah2 = $db->query("DELETE FROM tbs_fee_produk WHERE no_reg = '$no_reg' ");
 }
 
 
-$fee_produk = $db->query("SELECT * FROM laporan_fee_produk WHERE no_reg = '$no_reg' ");
+$fee_produk = $db->query("SELECT no_faktur, no_reg, no_rm, nama_petugas, kode_produk, nama_produk, jumlah_fee, tanggal, jam FROM laporan_fee_produk WHERE no_reg = '$no_reg' ");
 
 
 while ($data_fee = mysqli_fetch_array($fee_produk)){
