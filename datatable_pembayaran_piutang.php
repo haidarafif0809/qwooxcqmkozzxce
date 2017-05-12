7fd8b8c4d460235c456b0bd24342f94bb183d98c
Rindang Ramadhan <rindangramadhan10@gmail.com>
2017-05-12 11:10:02 +0700

    Review Implemntor

--- datatable_pembayaran_piutang.php
+++ datatable_pembayaran_piutang.php
@@ -43,7 +43,7 @@ if( !empty($requestData['search']['value']) ) {   // if there is a search parame
 }
 $query=mysqli_query($conn, $sql) or die("datatable_pembayaran_hutang.php: get employees");
 $totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
-$sql.=" ORDER BY CONCAT(tanggal,' ',jam) DESC ".$requestData['order'][0]['dir']."  LIMIT ".$requestData['start']." ,".$requestData['length']."  ";
+$sql.=" ORDER BY CONCAT(tanggal,' ',jam) DESC  LIMIT ".$requestData['start']." ,".$requestData['length']."  ";
 /* $requestData['order'][0]['column'] contains colmun index, $requestData['order'][0]['dir'] contains order such as asc/desc  */	
 $query=mysqli_query($conn, $sql) or die("employee-grid-data.php: get employees");
 
