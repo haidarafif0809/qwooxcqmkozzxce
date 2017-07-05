9a431460834344a4514d82d97e0895d78ca71ac5
Rikopernando <rikopernando96@gmail.com>
2017-07-05 11:36:04 +0700

    include once  cache   di edit penjualan
    include once cache  di edit penjualan

--- form_edit_penjualan_ugd.php
+++ form_edit_penjualan_ugd.php
@@ -632,7 +632,7 @@ Level 7
        <option value="">SILAKAN PILIH</option>
         <?php 
 
-        include 'cache.class.php';
+        include_once 'cache.class.php';
           $c = new Cache();
           $c->setCache('produk');
           $data_c = $c->retrieveAll();
