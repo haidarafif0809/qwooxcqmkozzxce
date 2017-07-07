1f9f2860eeefc5a6521ae143bc892058add98348
hendriandaglos <hendri.syand@gmail.com>
2017-07-07 09:57:40 +0700

    Input Hasil Laboratorium
    Hilangkan Fade Modal, dan pembenahan Query Modal Input Hasil Rawat Jalan

--- modal_pasien_pemeriksaan_lab_rawat_jalan.php
+++ modal_pasien_pemeriksaan_lab_rawat_jalan.php
@@ -21,50 +21,68 @@ $data_setting = mysqli_fetch_array($cek_setting);
 $hasil_setting = $data_setting['nama']; //jika hasil 1 maka = input hasil baru bayar, jika 0 maka = bayar dulu baru input hasil
 
 if($hasil_setting == '1'){
-//Query Rawat Jalan
-$sql = "SELECT reg.no_reg, reg.no_rm, reg.nama_pasien, reg.jenis_pasien, reg.tanggal, reg.dokter, reg.jenis_kelamin, pj.no_faktur, reg.id, us.id AS id_dokter";
-$sql.=" FROM registrasi reg INNER JOIN tbs_aps_penjualan tap ON reg.no_reg = tap.no_reg LEFT JOIN penjualan pj ON reg.no_reg = pj.no_reg LEFT JOIN user us ON reg.dokter = us.nama";
-$sql.=" WHERE (reg.status = 'Proses' OR reg.status = 'Rujuk Keluar Ditangani') AND reg.status_lab != '1' AND pj.no_faktur IS NULL GROUP BY reg.no_reg";
+
+  // getting total number records without any search
+  $sql = "SELECT COUNT(*) AS jumlah_data ";
+  $sql.=" FROM registrasi";
+  $sql.=" WHERE registrasi.jenis_pasien = 'Rawat Jalan' AND registrasi.status_lab != '1' AND (registrasi.status = 'Proses' OR registrasi.status = 'Rujuk Keluar Ditangani')";
+
+  $query = mysqli_query($conn, $sql) or die("Eror Sql 1: get employees");
+  $query_data = mysqli_fetch_array($query);
+  $totalData = $query_data['jumlah_data'];
+  $totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
+
+  //Query Rawat Jalan
+  $sql = "SELECT reg.no_reg, reg.no_rm, reg.nama_pasien, reg.jenis_pasien, reg.tanggal, pj.no_faktur, reg.id";
+  $sql.=" FROM registrasi reg INNER JOIN tbs_aps_penjualan tap ON reg.no_reg = tap.no_reg LEFT JOIN penjualan pj ON reg.no_reg = pj.no_reg ";
+  $sql.=" WHERE (reg.status = 'Proses' OR reg.status = 'Rujuk Keluar Ditangani') AND reg.jenis_pasien = 'Rawat Jalan' AND reg.status_lab != '1' AND pj.no_faktur IS NULL GROUP BY reg.no_reg";
   
 }
 else{
-//Query Rawat Jalan
-$sql = "SELECT reg.no_reg, reg.no_rm, reg.nama_pasien, reg.jenis_pasien, reg.tanggal, reg.dokter, reg.jenis_kelamin, pj.no_faktur, reg.id, us.id AS id_dokter";
-$sql.=" FROM registrasi reg INNER JOIN tbs_aps_penjualan tap ON reg.no_reg = tap.no_reg LEFT JOIN penjualan pj ON reg.no_reg = pj.no_reg LEFT JOIN user us ON reg.dokter = us.nama";
-$sql.=" WHERE reg.jenis_pasien = 'Rawat Jalan' AND reg.status = 'Sudah Pulang' AND reg.status_lab != '1' AND pj.no_faktur != '' GROUP BY reg.no_reg";
+
+  // getting total number records without any search
+  $sql = "SELECT COUNT(*) AS jumlah_data ";
+  $sql.=" FROM registrasi";
+  $sql.=" WHERE registrasi.jenis_pasien = 'Rawat Jalan' AND registrasi.status = 'Sudah Pulang' AND registrasi.status_lab != '1' ";
+
+  $query = mysqli_query($conn, $sql) or die("Eror Sql 2: get employees");
+  $query_data = mysqli_fetch_array($query);
+  $totalData = $query_data['jumlah_data'];
+  $totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
+
+  //Query Rawat Jalan
+  $sql = "SELECT reg.no_reg, reg.no_rm, reg.nama_pasien, reg.jenis_pasien, reg.tanggal, pj.no_faktur, reg.id";
+  $sql.=" FROM registrasi reg INNER JOIN tbs_aps_penjualan tap ON reg.no_reg = tap.no_reg LEFT JOIN penjualan pj ON reg.no_reg = pj.no_reg";
+  $sql.=" WHERE reg.jenis_pasien = 'Rawat Jalan' AND reg.status = 'Sudah Pulang' AND reg.status_lab != '1' AND pj.no_faktur != '' GROUP BY reg.no_reg";
+
 }
 
-$query = mysqli_query($conn, $sql) or die("eror 1");
-$totalData = mysqli_num_rows($query);
-$totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
 
 if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
 
-$cek_tanggal =   validateDate($requestData['search']['value']);
+    $cek_tanggal =   validateDate($requestData['search']['value']);
 
-if ($cek_tanggal == true) {
-   $tanggal_cari = tanggal_mysql($requestData['search']['value']);
-}
-else {
-   $tanggal_cari = $requestData['search']['value'];
-}
+    if ($cek_tanggal == true) {
+       $tanggal_cari = tanggal_mysql($requestData['search']['value']);
+    }
+    else {
+       $tanggal_cari = $requestData['search']['value'];
+    }
 
-    $sql.=" AND (reg.no_reg = '".$requestData['search']['value']."'";  
-    $sql.=" OR reg.no_rm = '".$requestData['search']['value']."' ";
-    $sql.=" OR reg.nama_pasien LIKE '".$requestData['search']['value']."%'";   
-    $sql.=" OR reg.tanggal = '".$tanggal_cari."' )";
+        $sql.=" AND (reg.no_reg = '".$requestData['search']['value']."'";  
+        $sql.=" OR reg.no_rm = '".$requestData['search']['value']."' ";
+        $sql.=" OR reg.nama_pasien LIKE '".$requestData['search']['value']."%'";   
+        $sql.=" OR reg.tanggal = '".$tanggal_cari."' )";
+  
+    $query=mysqli_query($conn, $sql) or die("Eror Search: get employees");
 
-}
-
-
-$query=mysqli_query($conn, $sql) or die("eror 2");
 $totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
-        
-$sql.=" ORDER BY reg.id ".$requestData['order'][0]['dir']."  LIMIT ".$requestData['start']." ,".$requestData['length']."   ";
+}
 
-/* $requestData['order'][0]['column'] contains colmun index, $requestData['order'][0]['dir'] contains order such as asc/desc  */    
-$query=mysqli_query($conn, $sql) or die("eror 3");
 
+$sql.=" ORDER BY reg.id DESC  LIMIT ".$requestData['start']." ,".$requestData['length']."   ";
+/* $requestData['order'][0]['column'] contains colmun index, $requestData['order'][0]['dir'] contains order such as asc/desc  */  
+$query=mysqli_query($conn, $sql) or die("Eror Last: get employees");
 
 $data = array();
 while( $row=mysqli_fetch_array($query) ) {  // preparing an array
