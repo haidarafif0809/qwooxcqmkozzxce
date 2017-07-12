09fb68515deaa20e56fd9affa71d432112599245
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-07-12 10:55:44 +0700

    Masukkan file print this js ke header

--- header.php
+++ header.php
@@ -141,7 +141,8 @@
 		<script src='jquery.elevatezoom.js'></script>
 		<!-- untuk operasi indexeddb -->
 		 <script src="https://unpkg.com/dexie@latest/dist/dexie.js"></script>
-		   <script src="jquery.printPage.js" type="text/javascript"></script>
+
+		   <script src="js/printThis.js" type="text/javascript"></script>
 
                
 </head>
