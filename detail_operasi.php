a204c2371363f6c2e2061c1d0eb77fb228f38dda
hendriandaglos <hendri.syand@gmail.com>
2017-07-19 16:56:50 +0700

    Request
    Jika Operasi, sub operasi dan detail operasi tersebut sudah digunakan tidak bisa di hapus kembali, tapi bisa di edit

--- detail_operasi.php
+++ detail_operasi.php
@@ -238,10 +238,22 @@ else{
   echo "<td> </td>";
 }
 
-if ($detail_sub_operasi['detail_sub_operasi_hapus'] > 0) {
-  echo "<td> <button class='btn btn-danger delete' data-id='". $data['id_detail_operasi'] ."'
+
+
+  //query jika hasil DETAIL operasi sudah di gunakan tidak bisa di hapus !!
+  $query_hasil_operasi = $db->query("SELECT id_detail_operasi FROM hasil_detail_operasi WHERE id_detail_operasi = '$data[id_detail_operasi]'");
+  $data_operasi = mysqli_num_rows($query_hasil_operasi);
+  if($data_operasi > 0){
+    $keterangan = 'Sudah dipakai';
+    echo " <td style='color: red'>". $keterangan." </td>";
+
+  }
+  else{
+    if ($detail_sub_operasi['detail_sub_operasi_hapus'] > 0) {
+    echo "<td> <button class='btn btn-danger delete' data-id='". $data['id_detail_operasi'] ."'
      data-nama='". $data['nama_detail_operasi'] ."'> <span class='glyphicon glyphicon-trash'> </span> Hapus </button> </td>";
-}
+    }
+  }
      
 echo "</tr>";
       }
