fd7223b273bf9d245d32cc7dce5c5feebff5915b
RindangRamadhan <rindangramadhan10@gmail.com>
2017-08-23 11:40:00 +0700

    Query Baru
    File Untuk Update Waktu detail_penjualan

---
+++ query_update_waktu_detail_penjualan.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'db.php';
+	include 'sanitasi.php';
+
+	$dari_tanggal = stringdoang($_GET['dari_tanggal']);
+	$sampai_tanggal = stringdoang($_GET['sampai_tanggal']);
+
+	$query_penjualan = $db->query("SELECT no_faktur, tanggal, jam FROM penjualan WHERE tanggal >= '$dari_tanggal' AND tanggal <= '$sampai_tanggal'");
+	while ($data_penjualan = mysqli_fetch_array($query_penjualan)) {
+
+		$waktu_penjualan = $data_penjualan['tanggal']." ".$data_penjualan['jam'];
+
+
+		$update_waktu_detail_penjualan = $db->query("UPDATE detail_penjualan SET waktu = '$waktu_penjualan' WHERE no_faktur = '$data_penjualan[no_faktur]' AND DATE(waktu) != '$data_penjualan[tanggal]' ");
+	}
+
+	echo "SELESAI UPDATE WAKTU DETAIL PENJUALAN";
+?>
