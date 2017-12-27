b53282390b871c64aea2266f88b4553dbdf09d7f
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-12-27 11:25:39 +0700

    order by id untuk menentukan no rm nya di ubah bukan pakai id lagi
    sebelumnya untuk menentukan berapa no rm nya di order pakai id , sekarang pakai substr dan convert dari kode pelanggannya

--- proses_pendaftaran_pasien_online_rj.php
+++ proses_pendaftaran_pasien_online_rj.php
@@ -48,7 +48,7 @@ $token            = stringdoang(urldecode($_GET['token']));
 
 $perusahaan      = $db->query("SELECT nama_perusahaan FROM perusahaan LIMIT 1")->fetch_array();
 $kode_klinik_reg = $db->query("SELECT kode_klinik FROM kode_klinik_reg LIMIT 1 ")->fetch_array();
-$query_pelanggan = $db_pasien->query("SELECT kode_pelanggan FROM pelanggan WHERE (kode_pelanggan != '0' OR kode_pelanggan IS NULL OR kode_pelanggan != '') AND kode_klinik =  '" . $kode_klinik_reg["kode_klinik"] . "' ORDER BY id DESC LIMIT 1 ")->fetch_array();
+$query_pelanggan = $db_pasien->query("SELECT kode_pelanggan FROM pelanggan WHERE (kode_pelanggan != '0' OR kode_pelanggan IS NULL OR kode_pelanggan != '') AND kode_klinik =  '" . $kode_klinik_reg["kode_klinik"] . "' ORDER BY CONVERT(SUBSTRING(kode_pelanggan,5,99),decimal) DESC LIMIT 1 ")->fetch_array();
 
 $angka_no_rm = str_replace($kode_klinik_reg['kode_klinik'] . '-', "", $query_pelanggan['kode_pelanggan']);
 $no_rm       = $angka_no_rm + 1;
